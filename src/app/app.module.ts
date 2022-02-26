276ad8b0f7c500b5061a3df2aefeb649419d2c10
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2022-02-25 22:37:14 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotePageComponent } from './components/note-page/note-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotePageComponent
   ],
   imports: [
     BrowserModule,
